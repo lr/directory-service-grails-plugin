57d63788ae23809559534b29274e1f042598006c
Lucas Rockwell <lr@lucasrockwell.com>
2012-07-22 12:42:27 -0700

    Updated comments, and provided a basic descripting of the class.

--- src/groovy/grails/plugins/directoryservice/DirectoryServiceEntry.groovy
+++ src/groovy/grails/plugins/directoryservice/DirectoryServiceEntry.groovy
@@ -27,8 +27,12 @@ import com.unboundid.ldap.sdk.SearchResultEntry
 import org.apache.log4j.Logger
 
 /**
- *
- *
+ * When DirectoryService returns a result set, it wraps each result object in
+ * a DirectoryServiceEntry object. This class allows you to interact with an
+ * UnboundID SearchResultEntry as if it were a writable Entry. In fact, when
+ * it is constructed with a SearchResultEntry, it duplicates the entry which
+ * results in an UnboundID Entry object, which you can update. The updating
+ * is done by overriding {@code propertyMissing(String name, value)}.
  *
  * @author Lucas Rockwell
  */
@@ -87,34 +91,54 @@ class DirectoryServiceEntry {
     }
     
     /**
-     * For modifying the object.
+     * This propertyMissing gets invoked when a value is supplied with the
+     * property name, so we override it so that we can set values in the 
+     * entry object. We also set the mods map, and update the modifications
+     * ArrayList.
      */
     def propertyMissing(String name, value) {
-        println "value in propertyMissing: ${value}"
-        mods[name] = value
-        
-        if (value) {
-            if (value instanceof String || value instanceof String[]) {
-                entry.setAttribute(name, value)
+        if (entry) {
+            mods[name] = value
+            if (value) {
+                if (value instanceof String || value instanceof String[]) {
+                    entry.setAttribute(name, value)
+                }
+                else {
+                    entry.setAttribute(name, 
+                        value.toArray(new String[value.size()]))
+                }
             }
             else {
-                entry.setAttribute(name, value.toArray(new String[value.size()]))
+                if (entry.getAttributeValue(name)) {
+                    entry.remoteAttribute(name)
+                }
             }
-        }
-        else {
-            if (entry.getAttributeValue(name)) {
-                entry.remoteAttribute(name)
+            // Now update the modifications list
+            if (value instanceof String || value instanceof String[]) {
+                updateModifications(name, value)
             }
-        }
-        // Now update the modifications list
-        if (value instanceof String || value instanceof String[]) {
-            updateModifications(name, value)
-        }
-        else {
-            updateModifications(name, value.toArray(new String[value.size()]))
+            else {
+                updateModifications(name, 
+                    value.toArray(new String[value.size()]))
+            }
         }
     }
     
+    /**
+     * Intercepts the following methods:
+     *
+     * <ul>
+     *   <li>getAttributeValues()</li>
+     *   <li>*AsDate()</li>
+     *   <li>*AsBoolean()</li>
+     *   <li>*AsLong()</li>
+     *   <li>dn()</li>
+     * </ul>
+     *
+     * These need to be called as methods instead of properties because with
+     * LDAP you never know if someone is going to name their attribute with one
+     * of these names at the end, so it is safest to force them to be methods.
+     */
     def methodMissing(String name, args) {
         if (entry) {
             if (name.matches(/^getAttributeValues?$/)) {
@@ -145,15 +169,26 @@ class DirectoryServiceEntry {
     
     /**
      * Discards any changes made to the object. Reinitializes both the
-     * {@code mods} map and the {@code modifications} array.
+     * {@code mods} map and the {@code modifications} array, and sets
+     * the entry as a duplicate of the searchResultEntry.
      */
     def discard() {
         mods = [:]
         modifications = new ArrayList<Modification>()
+        entry = searchResultEntry.duplicate()
     }
     
     /**
+     * Checks to see if this object has been modified. You can also pass in
+     * an optional attribute name to check to see if just that one attribute
+     * has been modified.
      *
+     * Note: This method is named isDirty to match GORM.
+     *
+     * @param attribute         Optional attribute name to check to see if it
+     * has been modified.
+     * @return {@code true} if this object (or the supplied attribute) has been
+     * modified, and {@code false} otherwise.
      */
     def isDirty(attribute=null) {
         if (mods) {
@@ -165,6 +200,22 @@ class DirectoryServiceEntry {
         return false
     }
     
+    /**
+     * Updates the modifications ArrayList with the UnboundID Modification
+     * that will be used when the entry is updated in the directory.
+     *
+     * This is called by the propertyMissing(String name, value) method, so
+     * there is no need to call it directly. However, if you want to
+     * short-circuit propertyMissing and update a bunch of attributes and then
+     * immediately save, you can use this method directly. But understand that
+     * the mods map will not be updated, and isDirty() will no know that you
+     * have modified the object.
+     *
+     * @param name              The name of the attribute for the modification.
+     * @param values            One or more String values to apply to the
+     * supplied attribute name.
+     * @see #propertyMissing(String name, value)
+     */
     def updateModifications(String name, String... values) {
         if (searchResultEntry) {
             def exists = false
@@ -172,12 +223,10 @@ class DirectoryServiceEntry {
                 if (obj.getAttributeName()?.equalsIgnoreCase(name)) {
                     exists = true
                     if (values) {
-                        println "we have values..."
                         modifications.set(i, new Modification(
                                 ModificationType.REPLACE, name, values))
                     }
                     else {
-                        println "nope..."
                         modifications.set(i, new Modification(
                             ModificationType.DELETE, name))
                     }
@@ -185,12 +234,10 @@ class DirectoryServiceEntry {
             }
             if (!exists) {
                 if (values) {
-                    println "values..."
                     modifications.add(new Modification(
                         ModificationType.REPLACE, name, values))
                 }
                 else {
-                    println "no values..."
                     modifications.add(new Modification(
                         ModificationType.DELETE, name))
                 }
