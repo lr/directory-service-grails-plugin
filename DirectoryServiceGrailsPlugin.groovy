2a17fcf05f61b3e34e820b79052e285cf8971948
Lucas Rockwell <lr@lucasrockwell.com>
2013-01-28 22:38:21 -0800

    Changed version to 0.7.0.

--- DirectoryServiceGrailsPlugin.groovy
+++ DirectoryServiceGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class DirectoryServiceGrailsPlugin {
     // the plugin version
-    def version = "0.6.1"
+    def version = "0.7.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
