8958f7dc9cd739794fecb3a9cf49b081a4705f27
Lucas Rockwell <lucas@cirrusidentity.com>
2014-04-02 10:02:45 -0400

    For issue #31, updated version to 0.10.1.

--- DirectoryServiceGrailsPlugin.groovy
+++ DirectoryServiceGrailsPlugin.groovy
@@ -1,9 +1,9 @@
 class DirectoryServiceGrailsPlugin {
-    def version = "0.10.0"
+    def version = "0.10.1"
     def grailsVersion = "2.0 > *"
     def title = "Directory Service Plugin"
     def description = 'Makes interacting with directory (LDAP) servers a breeze'
-    def documentation = "http://lr.github.com/directory-service/"
+    def documentation = "http://lr.github.io/directory-service/"
     def license = "Apache2"
     def developers = [ [ name: "Lucas Rockwell", email: "lr@lucasrockwell.com" ]]
     def issueManagement = [ system: "GitHub", url: "https://github.com/lr/directory-service/issues" ]
